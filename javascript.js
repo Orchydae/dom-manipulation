3df750aa8a0350852b433c5bb11f3b66be0df666
David N <nguyen.dddavid@hotmail.com>
2024-01-09 12:03:23 -0500

    First commit
    Add buttons and event listeners to index.html and javascript.js

--- javascript.js
+++ javascript.js
@@ -16,3 +16,38 @@ p.textContent = 'ME TOO!';
 div.appendChild(h1);
 div.appendChild(p);
 document.body.appendChild(div);
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert('Hello World (version 2)');
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+  alert('Hello World (version 3)');
+});
+
+function alertFunction() {
+  alert('YAY! YOU DID IT!');
+}
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', alertFunction);
+
+const btn4 = document.querySelector('#btn4');
+btn4.addEventListener('click', function (e) {
+    console.log(e);
+});
+
+btn4.addEventListener('click', function (e) {
+    console.log(e.target);
+});
+
+btn4.addEventListener('mouseover', function (e) {
+    e.target.style.background = 'blue';
+});
+
+const buttons = document.querySelectorAll('#container2 button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
